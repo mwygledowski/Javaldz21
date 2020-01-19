4140869a4642baa7e9d13576b46c24d08863766d
Marcin Wygledowski <wygledowski.marcin@gmail.com>
2020-01-19 13:00:05 +0100

    example-servlet project

---
+++ servlet-example/src/main/java/pl/sda/LogExampleServlet.java
@@ -0,0 +1,34 @@
+package pl.sda;
+
+import org.apache.log4j.Logger;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/LogExampleServlet")
+public class LogExampleServlet extends HttpServlet {
+
+    final static Logger logger = Logger.getLogger(LogExampleServlet.class);
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        PrintWriter out = resp.getWriter();
+        out.println("<h3>Log test<h3>");
+        logTest();
+    }
+
+    private void logTest() {
+        logger.trace("Trace Message!");
+        logger.debug("Debug Message!");
+        logger.info("Info Message!");
+        logger.warn("Warn Message!");
+        logger.error("Error Message!");
+        logger.fatal("Fatal Message!");
+    }
+}
---
+++ servlet-example/src/main/java/pl/sda/MyFirstServlet.java
@@ -0,0 +1,24 @@
+package pl.sda;
+
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/MyFirstServlet")
+public class MyFirstServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        PrintWriter writer = resp.getWriter();
+
+        for (int i = 0; i < 10 ; i++) {
+            writer.println("<h3>Hello World from servlet " + i + "</h3>");
+        }
+    }
+}
